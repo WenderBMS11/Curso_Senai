5138e4ea96e2d02bd536a214457c35f96501e3e8
WenderBMS11 <51210925+WenderBMS11@users.noreply.github.com>
2019-06-05 14:47:37 -0300

    arrumado erro de get duplicado

--- curso_senai/src/main/java/com/wender/senai/domain/enuns/TipoCliente.java
+++ curso_senai/src/main/java/com/wender/senai/domain/enuns/TipoCliente.java
@@ -1,5 +1,41 @@
 package com.wender.senai.domain.enuns;
 
 public enum TipoCliente {
+	// Será nosso estático
+	PESSOAFISICA(1, "Pessoa Física"), PESSOAJURIDICA(2, "Pessoa Juridica");
+
+	// Nossas atributos
+	private int cod;
+	private String descricao;
+
+	// Construtor
+	private TipoCliente(int cod, String descricaop) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	// gettes
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+
+	}
+
+	public static TipoCliente toEnum(Integer cod) {
+
+		if (cod == null) {
+			return null;
+		}
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+
+		throw new IllegalArgumentException("id Inválido" + cod);
+	}
 
 }
